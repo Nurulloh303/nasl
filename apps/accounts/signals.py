e51f65f8ebe0f0c51150566c4d6e8f7af17ff981
Nurulloh <nurullohmakhmudaliyev@gmail.com>
2026-04-19 15:25:39 +0500

    fbdsj

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -9,4 +10,13 @@ User = get_user_model()
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance, full_name=getattr(instance, "first_name", "") or instance.username)
+        # Yangi foydalanuvchiga welcome bonus tokenlar beramiz
+        bonus = getattr(settings, "WELCOME_BONUS_TOKENS", 5)
+        plan = Profile.PLAN_PRO if bonus > 0 else Profile.PLAN_FREE
+
+        Profile.objects.create(
+            user=instance,
+            full_name=getattr(instance, "first_name", "") or instance.username,
+            credits=bonus,
+            plan=plan,
+        )
